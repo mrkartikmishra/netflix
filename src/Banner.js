7223ad6b5de3f07940dc699fc7106d3f0964f899
Kartik Mishra <kartik.mishra@mtxb2b.com>
2021-08-09 18:51:26 +0530

    navbar and banner implemented

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axiosConfig from './axiosConfig';
 import requestUrls from './api-request-urls';
+import './Banner.css';
 
 const Banner = () => {
     const [movie, setMovie] = useState({});
@@ -18,11 +19,35 @@ const Banner = () => {
 
     }, []);
 
+    function truncateStr(str, n) {
+        return str?.length > n ? str.substr(0, n-1) + '...' : str;
+    }
+
     console.log("movie==>>", movie);
 
-    return(
-        <header>
+    console.log(`("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`);
 
+    return(
+        <header className="banner"
+            style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center center',
+                backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`
+            }}
+        >
+            <div className="banner__container">
+                <h1 className="banner__container__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className="banner__container__buttons">
+                    <button className="banner__container__button">
+                        Play
+                    </button>
+                    <button className="banner__container__button">
+                        My List
+                    </button>
+                </div>
+                <h1 className="banner__container__overview">{truncateStr(movie?.overview, 150)}</h1>
+            </div>
+            <div className="banner__fade" />
         </header>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import './Navbar.css';
+
+import Avatar from './images/1.png';
+
 const Navbar = () => {
     return(
-        <div>
-            Navbar
+        <div className="nav">
+            <img className="nav__logo" src="http://pngimg.com/uploads/netflix/netflix_PNG11.png" alt="Netflix logo" />
+            <img className="nav__avatar" src={Avatar} alt="Avatar" />
         </div>
     );
 }
